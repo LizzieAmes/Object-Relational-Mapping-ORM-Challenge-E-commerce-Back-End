e76fb48f1f5ff1191d5bb07b5ef3235a3072670e
Lizzie <lizziejacoboo@gmail.com>
2024-01-30 11:08:30 -0700

    product.js, index.js, category-routes.js, server.js

--- models/index.js
+++ models/index.js
@@ -4,25 +4,21 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-<<<<<<< HEAD
 // Products belongsTo Category  
 Product.belongsTo(Category, { foreignKey: 'category_id'});
 
 // Categories have many Products
 Category.hasMany(Product, { foreignKey: 'category_id'});  
-=======
-// Products belongsTo Category
-
-// Categories have many Products
->>>>>>> 4417f3c481f9a9828b84b9da613785fd6e2c32c6
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id' });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' })
 
 module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };
